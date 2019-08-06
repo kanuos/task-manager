3f5e6e165032c1f345bb0bca04fbc75044bacfca
kanuos <sounakmukherjee@ymail.com>
2019-08-06 12:36:18 -0400

    Updates

---
+++ precache-manifest.0b1fb72e9b89977bce5c69cf6a8c8859.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac4afe580a603da7c0617feb7851416",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "2006ca72c22da616e1d9",
+    "url": "/task-manager/static/js/2.4366c3cf.chunk.js"
+  },
+  {
+    "revision": "1bb7cc1e75dbb1c8e4ea",
+    "url": "/task-manager/static/js/main.b9c8bbbd.chunk.js"
+  },
+  {
+    "revision": "83a345ca91bebf8c2b25",
+    "url": "/task-manager/static/js/runtime~main.2231b73b.js"
+  }
+]);
